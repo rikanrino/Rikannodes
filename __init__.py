63c77f883203e14244c13d04bc4b1eda680891b0
rikanrino <rikanrino@gmail.com>
2026-05-08 01:14:58 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,32 +1,24 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-from .nodes import RikanPromptRelayEncodeTimeline, RikanPromptRelayLoraGate, RikanPromptRelayPowerLoraGate
+from .nodes import RikanPromptRelayEncodeTimeline
 from comfy_api.latest import ComfyExtension, io
 from typing_extensions import override
-
 
 class PromptRelay(ComfyExtension):
     @override
     async def get_node_list(self) -> list[type[io.ComfyNode]]:
         return [
-            PromptRelayEncodeTimeline,
-            PromptRelayLoraGate,
-            PromptRelayPowerLoraGate,
+            RikanPromptRelayEncodeTimeline,
         ]
-
 
 async def comfy_entrypoint() -> PromptRelay:
     return PromptRelay()
 
 NODE_CLASS_MAPPINGS = {
     "RikanPromptRelayEncodeTimeline": RikanPromptRelayEncodeTimeline,
-    "RikanPromptRelayLoraGate": RikanPromptRelayLoraGate,
-    "RikanPromptRelayPowerLoraGate": RikanPromptRelayPowerLoraGate, 
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RikanPromptRelayEncodeTimeline": "Rikan Prompt Relay Encode (Timeline)",
-    "RikanPromptRelayLoraGate": "Rikan Prompt Relay LoRA Gate",
-    "RikanPromptRelayPowerLoraGate": "Rikan Prompt Relay Power Lora Gate",
 }
 
 
