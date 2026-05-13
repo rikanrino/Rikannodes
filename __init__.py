af53f1122d4bee4f959217f6b1a5465fa42e5cfb
rikanrino <rikanrino@gmail.com>
2026-05-13 21:16:03 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,6 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-from .nodes import RikanPromptRelayEncodeTimeline
 from comfy_api.latest import ComfyExtension, io
 from typing_extensions import override
-
-class PromptRelay(ComfyExtension):
-    @override
-    async def get_node_list(self) -> list[type[io.ComfyNode]]:
-        return [
-            RikanPromptRelayEncodeTimeline,
-        ]
-
-async def comfy_entrypoint() -> PromptRelay:
-    return PromptRelay()
-
-NODE_CLASS_MAPPINGS = {
-    "RikanPromptRelayEncodeTimeline": RikanPromptRelayEncodeTimeline,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "RikanPromptRelayEncodeTimeline": "Rikan Prompt Relay Encode (Timeline)",
-}
-
 
 WEB_DIRECTORY = "./web"
 
